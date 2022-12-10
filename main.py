1d495df5d3bd313b5a1c4fc6e56d53bee4185a65
sadam <sadamccr@gmail.com>
2022-12-11 01:30:07 +0500

    done

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 # class Dog(Animal):
 #     def bark(self):
 #         return 'Woof!'
-#
+
 #
 # class Cat(Animal):
 #     def meow(self):
@@ -22,12 +22,12 @@
 # class Robot:
 #     def move(self):
 #         return 'moves..'
-#
+
 #
 # class CleanRobot(Robot):
 #     def clean(self):
 #         return 'Clean!'
-#
+
 #
 # class PlayRobot(Robot):
 #     def play(self):
@@ -37,3 +37,51 @@
 # myrebot = PlayRobot()
 # print(myrebot.play())
 # print(myrebot.move())
+
+
+# #make a super robot with lots of mixed abilities
+# # --------- COMPOSITION -----------------
+
+
+# class Dog:
+#     def bark(self):
+#         return 'Woof!'
+#
+#
+# class Robot:
+#     def move(self):
+#         return 'moves..'
+#
+#
+# class CleanRobot:
+#     def clean(self):
+#         return 'Clean!'
+#
+#
+# class SuperRobot:
+#     def __init__(self):
+#         self.ob1 = Dog()
+#         self.ob2 = Robot()
+#         self.ob3 = CleanRobot()
+#
+#     def play_game(self):
+#         return 'Chess is best!'
+#
+#     def bark(self):
+#         return self.ob1.bark()
+#
+#     def move(self):
+#         return self.ob2.move()
+#
+#     def clean(self):
+#         return self.ob3.clean()
+#
+#
+# henry = SuperRobot()
+# print(henry.bark())
+# print(henry.move())
+# print(henry.clean())
+#
+# # #------ Conclusion ----------
+# # # Use composition when you can
+# # # Use inheritance when you must
