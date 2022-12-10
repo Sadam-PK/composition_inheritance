70a18a8231dd63986895512eb6ac11b11f059dee
sadam <sadamccr@gmail.com>
2022-12-11 01:15:30 +0500

    inheritance example

---
+++ main.py
@@ -0,0 +1,18 @@
+# class Animal:
+#     def eat(self):
+#         return 'Yum..'
+#
+#
+# class Dog(Animal):
+#     def bark(self):
+#         return 'Woof!'
+#
+#
+# class Cat(Animal):
+#     def meow(self):
+#         return 'Meow..'
+#
+#
+# snoopy = Dog()
+# print(snoopy.bark())
+# print(snoopy.eat())
