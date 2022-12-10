cc0c1392cc8b76a63c1e95df63cbf4a8095ce057
sadam <sadamccr@gmail.com>
2022-12-11 01:16:02 +0500

    inheritance example Robot

--- main.py
+++ main.py
@@ -16,3 +16,24 @@
 # snoopy = Dog()
 # print(snoopy.bark())
 # print(snoopy.eat())
+
+
+#
+# class Robot:
+#     def move(self):
+#         return 'moves..'
+#
+#
+# class CleanRobot(Robot):
+#     def clean(self):
+#         return 'Clean!'
+#
+#
+# class PlayRobot(Robot):
+#     def play(self):
+#         return 'play..'
+#
+#
+# myrebot = PlayRobot()
+# print(myrebot.play())
+# print(myrebot.move())
